66a4aab2fb34232930f55792723683ead8571438
Badet Maxime <badet.maxime@gmail.com>
2022-03-30 17:36:07 +0200

    olerobot mise en place strategie puissance 4

--- src/controleur/ControleurPuissanceOrdi.java
+++ src/controleur/ControleurPuissanceOrdi.java
@@ -20,38 +20,4 @@ public class ControleurPuissanceOrdi extends ControleurPuissance{
         joueur2=new Ordinateur();
 
     }
-
-    protected boolean testRotation(Grille g,Joueur j) throws CoupInvalideException {
-        boolean test=false;
-        for(int i=0;i<2;i++){
-            g.gererRotation(i);
-            Set<Jeton> lesJetons=g.partieTerminee();
-            if(lesJetons.size()==1 &&  gagnantPartie(lesJetons).equals(j) ){
-                test=true;
-                break;
-            }
-        }
-        return test;
-    }
-    @Override
-    protected void traiterCoup (Joueur joueur)  throws CoupInvalideException {
-        if( joueur instanceof Humain){
-            super.traiterCoup(joueur);
-        }
-        else{
-            Grille grilleTest=
-            testRotation(Grille g,Joueur j)
-
-        }
-        if(nbRotation.get(joueur)>=0){
-            int choix=((IhmPuissance)ihm).choixMouvement(nbRotation.get(joueur));
-            if(choix==1/*avec*/){
-                traiterCoupavecRotation(joueur);
-                return;
-            }
-        }
-        List<Integer> coup=(ihm.demanderCoup());
-        Coup c=new CoupPuissance(coup.get(0),jetonDuJoueur.get(joueur));
-        plateau.gererCoup(c);
-    }
 }
